ba86e1050f2ea41f73430949cdd3e557b4360ea4
Andres :) <166577464+Porras266@users.noreply.github.com>
2024-11-08 09:45:59 -0600

    Add files via upload

---
+++ Estructuras Multiples/Estructuras Multiples/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Estructuras_Multiples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblDiaDeLaSemana = new System.Windows.Forms.Label();
+            this.txtDiaDeLaSemana = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSalir);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.btnNuevo);
+            this.groupBox1.Controls.Add(this.txtNumero);
+            this.groupBox1.Controls.Add(this.lblNumero);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(322, 97);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese Datos";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(37, 27);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(78, 18);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Número :";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(135, 27);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(124, 20);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(22, 66);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(126, 66);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(226, 66);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtDiaDeLaSemana);
+            this.groupBox2.Controls.Add(this.lblDiaDeLaSemana);
+            this.groupBox2.Location = new System.Drawing.Point(13, 136);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(322, 70);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Día de la semana";
+            // 
+            // lblDiaDeLaSemana
+            // 
+            this.lblDiaDeLaSemana.AutoSize = true;
+            this.lblDiaDeLaSemana.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaDeLaSemana.Location = new System.Drawing.Point(19, 35);
+            this.lblDiaDeLaSemana.Name = "lblDiaDeLaSemana";
+            this.lblDiaDeLaSemana.Size = new System.Drawing.Size(129, 16);
+            this.lblDiaDeLaSemana.TabIndex = 0;
+            this.lblDiaDeLaSemana.Text = "Día de la semana";
+            // 
+            // txtDiaDeLaSemana
+            // 
+            this.txtDiaDeLaSemana.Enabled = false;
+            this.txtDiaDeLaSemana.Location = new System.Drawing.Point(155, 35);
+            this.txtDiaDeLaSemana.Name = "txtDiaDeLaSemana";
+            this.txtDiaDeLaSemana.Size = new System.Drawing.Size(161, 20);
+            this.txtDiaDeLaSemana.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(347, 218);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(363, 257);
+            this.MinimumSize = new System.Drawing.Size(363, 257);
+            this.Name = "Form1";
+            this.Text = "Estructuras Multiples";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtDiaDeLaSemana;
+        private System.Windows.Forms.Label lblDiaDeLaSemana;
+    }
+}
+
---
+++ Estructuras Multiples/Estructuras Multiples/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estructuras_Multiples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int Dia = int.Parse(txtNumero.Text);
+            if (Dia <= 0 || Dia > 7)
+            {
+                MessageBox.Show("Ingrese un valor entre 1 y 7");
+            }
+            if (Dia == 1)
+            {
+                txtDiaDeLaSemana.Text = "Lunes";
+            }
+            if (Dia == 2)
+            {
+                txtDiaDeLaSemana.Text = "Martes";
+            }
+            if (Dia == 3)
+            {
+                txtDiaDeLaSemana.Text = "Miercoles";
+            }
+            if (Dia == 4)
+            {
+                txtDiaDeLaSemana.Text = "Jueves";
+            }
+            if (Dia == 5)
+            {
+                txtDiaDeLaSemana.Text = "Viernes";
+            }
+            if (Dia == 6)
+            {
+                txtDiaDeLaSemana.Text = "sabado";
+            }
+            if (Dia == 7)
+            {
+                txtDiaDeLaSemana.Text = "Domingo";
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtDiaDeLaSemana.Clear();
+            txtNumero.Clear();
+            txtNumero.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("El programa se cerrara");
+            Close();
+        }
+    }
+}
